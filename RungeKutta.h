b03c733d9038535721d06e861deef713a6607500
Daniel Stahl <dstahl@glsllc.com>
2016-11-22 09:07:26 -0500

    improved functional

--- RungeKutta.h
+++ RungeKutta.h
@@ -7,7 +7,7 @@
 #include <iostream> //debugging
 
 
-class RungeKutta { //generic class, can take complex numbers, multivariate functions, etc
+class RungeKutta { //generic class, can take complex numbers etc
 	private:
 		int numSteps;
 		double t;
@@ -22,29 +22,29 @@ class RungeKutta { //generic class, can take complex numbers, multivariate funct
 			hlfh=h*.5;
 			sixthh=h/6.0;
 		}
-		template<typename T, typename FN, typename... ARGS>
-		std::vector<T> compute(FN&& fn, std::vector<T>&& initialValues, ARGS&&... args){ //note that move semantics are required...
+		template<typename T, typename FN>
+		std::vector<T> compute(FN&& fn, const std::vector<T>& initialValues){
 			int n=initialValues.size();
-			std::vector<T> newVal(n); //temporarily hold values
+			std::vector<T> newVal(initialValues); //temporarily hold values
 			for(int i=0; i<numSteps; i++){
-				std::vector<T> k1=fn(h*i, std::move(initialValues), args...);
-				for(int j=0; j<n;j++){
-					newVal[j]=initialValues[j]+k1[j]*hlfh;
+				std::vector<T> k1=fn(h*i, newVal);
+				for(int j=0; j<n;++j){
+					newVal[j]=newVal[j]+k1[j]*hlfh;
 				}
-				std::vector<T> k2=fn(h*i+hlfh, std::move(newVal), args...);
-				for(int j=0; j<n;j++){
-					newVal[j]=initialValues[j]+k2[j]*hlfh;
+				std::vector<T> k2=fn(h*i+hlfh, newVal);
+				for(int j=0; j<n;++j){
+					newVal[j]=newVal[j]+k2[j]*hlfh-k1[j]*hlfh;
 				}
-				std::vector<T> k3=fn(h*i+hlfh, std::move(newVal), args...);
-				for(int j=0; j<n;j++){
-					newVal[j]=initialValues[j]+k3[j]*h;
+				std::vector<T> k3=fn(h*i+hlfh, newVal);
+				for(int j=0; j<n;++j){
+					newVal[j]=newVal[j]+k3[j]*h-k2[j]*hlfh;
 				}
-				std::vector<T> k4=fn(h*i+h, std::move(newVal), args...);
-				for(int j=0; j<n;j++){
-					initialValues[j]=initialValues[j]+(k1[j]+k2[j]*2+k3[j]*2+k4[j])*sixthh;
+				std::vector<T> k4=fn(h*i+h, newVal);
+				for(int j=0; j<n;++j){
+					newVal[j]=newVal[j]+(k1[j]+k2[j]*2+k3[j]*2+k4[j])*sixthh;
 				}
 			}
-			return initialValues;
+			return newVal;
 		}
 };
 #endif
